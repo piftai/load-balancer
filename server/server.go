0ea44c45b0ab4ca0b0801b3696ac5d185046f167
piftai <piftaiwork@gmail.com>
2025-05-01 02:48:10 +0300

    server first version

---
+++ server/server.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"github.com/piftai/load-balancer/balancer"
+	"github.com/piftai/load-balancer/proxy"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func Start(port string, loadBalancer balancer.Balancer) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		backend := loadBalancer.Next()
+		if backend == nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			w.Write([]byte("No healthy backends available"))
+			return
+		}
+		target, _ := url.Parse(backend.URL)
+		prx := proxy.NewReverseProxy(target)
+		prx.ServeHTTP(w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
