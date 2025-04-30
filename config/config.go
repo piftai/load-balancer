ede3eb7961069c6453c106323de336687e84a0a1
piftai <piftaiwork@gmail.com>
2025-05-01 00:48:24 +0300

    config package added

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+)
+
+type Backend struct {
+	URL    string `yaml:"url"`
+	Weight int    `yaml:"weight"`
+}
+
+type Config struct {
+	Port     string    `yaml:"port" default:"8080"`
+	Backends []Backend `yaml:"backends"`
+}
+
+func Load() Config {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal("Failed to read config.yaml. Error:", err)
+	}
+
+	var config Config
+	if err = yaml.Unmarshal(data, &config); err != nil {
+		log.Fatal("Failed to parse config. Error:", err)
+	}
+
+	return config
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/piftai/load-balancer/config"
+)
+
+func main() {
+	cfg := config.Load()
+
+}
