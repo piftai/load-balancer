cbcbb18ad140d6282e396c43e279fa0e0a78cdda
piftai <piftaiwork@gmail.com>
2025-05-01 02:46:43 +0300

    first test was ok

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"github.com/piftai/load-balancer/balancer"
 	"github.com/piftai/load-balancer/config"
+	"github.com/piftai/load-balancer/server"
 )
 
 func main() {
 	cfg := config.Load()
-
+	var backends []balancer.Backend
+	for _, b := range cfg.Backends {
+		backends = append(backends, balancer.Backend{
+			URL:    b.URL,
+			Weight: b.Weight,
+			Alive:  true, // Изначально считаем все бэкенды живыми
+		})
+	}
+	loadBalancer := balancer.New(backends)
+	server.Start(cfg.Port, loadBalancer)
 }
