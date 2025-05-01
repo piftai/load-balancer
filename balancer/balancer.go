8e3ac61b3a35578c8772f59d05a55a09eaf174cf
piftai <piftaiwork@gmail.com>
2025-05-01 15:38:29 +0300

    refactoring + health checks

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -2,7 +2,11 @@
 package balancer
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"sync"
+	"time"
 )
 
 type Backend struct {
@@ -11,19 +15,59 @@ type Backend struct {
 	Alive  bool
 }
 
-type WRR struct {
-	backends []Backend
-	mu       sync.RWMutex
-	index    int // - индекс текущего бекенда
-	current  int // - потраченный вес
-	stopChan chan struct{}
+func (b *Backend) HealthCheck() {
+	client := http.Client{
+		Timeout: 1 * time.Second,
+	}
+	resp, err := client.Get(b.URL) // будем считать, что запрос по root url равноценен запросу /health
+	if err != nil {
+		log.Printf("Health check. Backend %v not answering", b.URL)
+		b.Alive = false
+		return
+	}
+	defer resp.Body.Close()
+
+	// Считаем бэкенд живым, если статус код 2xx или 3xx
+	b.Alive = resp.StatusCode < 400
 }
 
-func New(backends []Backend) *WRR {
+func HealthChecker(backends []*Backend, interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		var wg sync.WaitGroup
+
+		for _, backend := range backends {
+			wg.Add(1)
+			go func(b *Backend) {
+				defer wg.Done()
+				b.HealthCheck()
+				fmt.Printf("Backend %s is alive: %v\n", b.URL, b.Alive)
+			}(backend)
+		}
+
+		wg.Wait()
+
+	}
+}
+
+type WRR struct {
+	backends []*Backend
+	mu       sync.RWMutex
+	index    int           // - индекс текущего бекенда
+	current  int           // - потраченный вес
+	stopChan chan struct{} // для graceful shutdown
+}
+
+func New(backends []*Backend, healthInterval int) *WRR {
 	w := &WRR{
 		backends: backends,
 		stopChan: make(chan struct{}),
 	}
+
+	go HealthChecker(w.backends, time.Duration(healthInterval)*time.Second)
+
 	return w
 }
 
@@ -31,19 +75,19 @@ func (wr *WRR) Next() *Backend {
 	wr.mu.RLock()
 	defer wr.mu.RUnlock()
 	for i := 0; i < len(wr.backends); i++ {
-		backend := &wr.backends[wr.index]
+		backend := wr.backends[wr.index]
 		wr.current++
 
 		if wr.current >= backend.Weight { // проверяем, истратили ли мы вес текущего бекенда, если да, то переходим на следующий
-			wr.current = 0
-			wr.index = (wr.index + 1) % len(wr.backends)
+			wr.current = 0                               // обнуляем вес для следующего бекенда
+			wr.index = (wr.index + 1) % len(wr.backends) // берем следующий бекенд
 		}
 
 		if backend.Alive { // если жив, возвращаем сразу же его
 			return backend
 		}
 
-		wr.index = (wr.index + 1) % len(wr.backends)
+		wr.index = (wr.index + 1) % len(wr.backends) // если бекенд не отвечает, то сразу же идем на другой, а потом снова проверим его
 	}
 
 	return nil
