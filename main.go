a180b2625181ca1e52cda06ca7dd998b3a2172af
piftai <piftaiwork@gmail.com>
2025-05-01 15:39:44 +0300

    starting server

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	cfg := config.Load()
-	var backends []balancer.Backend
+	var backends []*balancer.Backend
 	for _, b := range cfg.Backends {
-		backends = append(backends, balancer.Backend{
+		backends = append(backends, &balancer.Backend{
 			URL:    b.URL,
 			Weight: b.Weight,
 			Alive:  true, // Изначально считаем все бэкенды живыми
 		})
 	}
-	loadBalancer := balancer.New(backends)
+	loadBalancer := balancer.New(backends, cfg.HealthTick)
 	server.Start(cfg.Port, loadBalancer)
 }
